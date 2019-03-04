20686231681dde75bfbb3855935c60e5969cd6dd
taulfsime <34596275+taulfsime@users.noreply.github.com>
2019-03-04 20:09:52 +0200

    Added Businesses
    Business for authors, books, genres and publishers

---
+++ Business/Businesses/BusinessBook.cs
@@ -0,0 +1,46 @@
+﻿using Data;
+using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Business.Bussiness
+{
+    public class BusinessBook
+    {
+        private cDbContext cDbContext;
+        
+        public void addNewBook(Book book)
+        {
+            using (cDbContext = new cDbContext())
+            {
+                cDbContext.books.Add(book);
+                cDbContext.SaveChanges();
+            }
+        }
+        
+        public Book getBook(int id)
+        {
+            using (cDbContext = new cDbContext())
+            {
+                foreach (Book book in cDbContext.books)
+                {
+                    if (id == book.id)
+                    {
+                        return book;
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("Book with this id does not exist!");
+        }
+        
+        public List<Book> getAllBooks()
+        {
+            using (cDbContext = new cDbContext())
+            {
+                return cDbContext.books.ToList();
+            }
+        }
+    }
+}
---
+++ Business/Businesses/BusinessGenre.cs
@@ -0,0 +1,46 @@
+﻿using Data;
+using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Business.Bussiness
+{
+    public class BusinessGenre
+    {
+        private cDbContext cDbContext;
+        
+        public void addNewGenre(Genre genre)
+        {
+            using (cDbContext = new cDbContext())
+            {
+                cDbContext.genres.Add(genre);
+                cDbContext.SaveChanges();
+            }
+        }
+        
+        public Genre getGenre(int id)
+        {
+            using (cDbContext = new cDbContext())
+            {
+                foreach (Genre genre in cDbContext.genres)
+                {
+                    if (id == genre.id)
+                    {
+                        return genre;
+                    }
+                }
+            }
+
+            throw new InvalidOperationException("Genre with this id does not exist!");
+        }
+        
+        public List<Genre> getAllGenres()
+        {
+            using (cDbContext = new cDbContext())
+            {
+                return cDbContext.genres.ToList();
+            }
+        }
+    }
+}
