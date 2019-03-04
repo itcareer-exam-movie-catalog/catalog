35bcd725c35aa40be208d34ae5bcea4cc4401345
taulfsime <34596275+taulfsime@users.noreply.github.com>
2019-03-04 19:03:12 +0200

    Added MySqlQueries and removed some files

---
+++ MySqlFiles/init.sql
@@ -0,0 +1,34 @@
+CREATE DATABASE IF NOT EXISTS catalog;
+USE catalog;
+
+CREATE TABLE IF NOT EXISTS `books`
+(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `title` varchar(128) NOT NULL,
+    `authorIds` varchar(128) NOT NULL,
+    `publisherId` int NOT NULL,
+    `pages` int NOT NULL,
+    `publicationYear` int NOT NULL,
+    `genreIds` varchar(128) NOT NULL,
+    `price` decimal(5, 2) NOT NULL,
+    `photo` LONGBLOB
+);
+
+CREATE TABLE IF NOT EXISTS `authors`
+(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `firstName` VARCHAR(128) NOT NULL,
+    `lastName` VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `publishers`
+(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `genres`
+(
+	`id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(128) NOT NULL
+);
