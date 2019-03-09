85d959d64fbc0ea991eb83e06d01979f3940a48b
Ivan Dosev <47494182+yeetlord420@users.noreply.github.com>
2019-03-09 21:26:52 +0200

    Update and rename cDbContext.cs to CatalogDbContext.cs

---
+++ Data/CatalogDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+
+    public class CatalogDbContext : DbContext
+    {
+        public CatalogDbContext() 
+            : base("name=CatalogDbContext") 
+            { 
+            }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Director> Directors { get; set; }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
