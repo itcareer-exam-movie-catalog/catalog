dc79862384218a30123c23808f0d0a16e5cc2bc9
taulfsime <34596275+taulfsime@users.noreply.github.com>
2019-03-10 21:19:36 +0200

    Fix connection problem and started ConsoleUI
    Co-Authored-By: vasilenad <vasilenad@users.noreply.github.com>

---
+++ ConsoleUIApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Businesses;
+
+namespace ConsoleUIApp
+{
+    class Program
+    {
+        public static BusinessActors businessActors = new BusinessActors();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(businessActors.getActor(1).firstName);
+        }
+    }
+}
