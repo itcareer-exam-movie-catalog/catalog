fa1e8794756717335a1c26603b382bc9816b3e87
Ivan Dosev <47494182+yeetlord420@users.noreply.github.com>
2019-03-18 19:35:52 +0200

    Update BusinessCategories.cs

--- Business/Businesses/BusinessCategories.cs
+++ Business/Businesses/BusinessCategories.cs
@@ -11,6 +11,22 @@ namespace Business.Businesses
         private CatalogDbContext database;
 
         /// <summary>
+        /// Constructor that reupdates the database context.
+        /// </summary>
+        public BusinessCategories(CatalogDbContext cDbContext)
+        {
+         database = cDbContext;   
+        }
+        
+        /// <summary>
+        /// Returns the database context.
+        /// </summary>
+        public CatalogDbContext GetCatalogDbContext()
+        {
+         return database;   
+        }
+        
+        /// <summary>
         /// Adds a new categoty to the database.
         /// </summary>
         /// <param name="category">The category</param>
