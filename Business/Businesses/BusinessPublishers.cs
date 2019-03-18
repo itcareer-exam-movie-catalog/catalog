b0121b7f6af0034cbe5d5013ce0278bd08d9ffe4
Ivan Dosev <47494182+yeetlord420@users.noreply.github.com>
2019-03-18 19:36:52 +0200

    Update BusinessPublishers.cs

--- Business/Businesses/BusinessPublishers.cs
+++ Business/Businesses/BusinessPublishers.cs
@@ -13,6 +13,22 @@ namespace Business.Businesses
         private CatalogDbContext database;
 
         /// <summary>
+        /// Constructor that reupdates the database context.
+        /// </summary>
+        public BusinessPublishers(CatalogDbContext cDbContext)
+        {
+         database = cDbContext;   
+        }
+        
+        /// <summary>
+        /// Returns the database context.
+        /// </summary>
+        public CatalogDbContext GetCatalogDbContext()
+        {
+         return database;   
+        }
+        
+        /// <summary>
         /// Adds a new publisher to the database.
         /// </summary>
         /// <param name="publisher">The publisher</param>
