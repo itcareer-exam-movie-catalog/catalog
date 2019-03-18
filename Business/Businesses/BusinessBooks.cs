bf3ffef28bff31e5ec76f7ac918aa0252f7c2092
Ivan Dosev <47494182+yeetlord420@users.noreply.github.com>
2019-03-18 19:48:12 +0200

    Update BusinessBooks.cs

--- Business/Businesses/BusinessBooks.cs
+++ Business/Businesses/BusinessBooks.cs
@@ -10,6 +10,10 @@ namespace Business.Businesses
     {
         private CatalogDbContext database;
 
+        public BusinessBooks()
+        {
+        }
+        
         /// <summary>
         /// Constructor that reupdates the database context.
         /// </summary>
