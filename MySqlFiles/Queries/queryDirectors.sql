ebcc0dc16f35b7d9f36493a03574048c48d3c0c5
Ivan Dosev <47494182+yeetlord420@users.noreply.github.com>
2019-04-03 18:15:27 +0300

    Add files via upload

---
+++ MySqlFiles/Queries/queryDirectors.sql
@@ -0,0 +1,18 @@
+USE catalog;
+INSERT INTO directors (firstName, lastName) VALUES
+("Mel", "Brooks"),
+("Harold", "Ramis"),
+("John", "Landis"),
+("John Hughes", "Rob Reiner"),
+("Ivan", "Reitman"),
+("Terry", "Jones"),
+("Michael", "Bay"),
+("John", "McTiernan"),
+("Tony", "Scott"),
+("James", "Cameron"),
+("Steven", "Spielberg"),
+("Peter", "Jackson"),
+("Ridley", "Scott"),
+("Rian", "Johnson"),
+("Billy", "Wilder"),
+("Stanley", "Kubrick")
